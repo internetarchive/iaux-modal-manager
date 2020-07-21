c436e5f1bab1b4cb9e0caf0bdd04ade47fb2301f
Jason Buckner <jason@jasonbuckner.com>
2020-07-21 14:45:29 -0700

    Export all the things

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export { ModalManagerController } from './src/modal-manager-controller';
-export { ModalManager } from './src/modal-manager';
-export { ModalTemplate } from './src/modal-template';
 export { ModalConfig } from './src/modal-config';
+export { ModalManagerController } from './src/modal-manager-controller';
+export { ModalManagerInterface, ModalManager } from './src/modal-manager';
+export { ModalTemplate } from './src/modal-template';
