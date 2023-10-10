3b210fc30d1cde9ab8af2f447601f74d9098407d
clau <1281581+dualcnhq@users.noreply.github.com>
2023-10-11 07:20:41 +0800

    Add gh-pages GH action config (#65)
    Co-authored-by: Isa Herico Velasco <7840857+iisa@users.noreply.github.com>

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '',
+  assetsInclude: ['**/*.jpg'],
+  build: {
+    outDir: 'ghpages',
+    manifest: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  server: {
+    host: true,
+    port: 8080,
+    open: true,
+    cors: true,
+  },
+});
